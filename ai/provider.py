474b3c85275a4411408b963dd1efb30369fdf308
izumi02-ui <rohitchakraborty0555@gmail.com>
2026-07-07 05:26:56 -0700

    Update provider.py

--- ai/provider.py
+++ ai/provider.py
@@ -1,1 +1,36 @@
+"""
+Provider Manager
 
+Responsible for choosing which AI
+provider should answer.
+
+Currently:
+
+Gemini
+
+Future:
+
+Groq
+OpenRouter
+OpenAI
+"""
+
+from ai.providers.gemini import GeminiProvider
+
+
+class ProviderManager:
+
+    def __init__(self):
+
+        self.provider = GeminiProvider()
+
+    async def ask(
+        self,
+        user_id: int,
+        message: str,
+    ):
+
+        return await self.provider.ask(
+            user_id,
+            message
+        )
