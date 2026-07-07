9fae836bdbaa62723f15691d821a78af3c169095
izumi02-ui <rohitchakraborty0555@gmail.com>
2026-07-07 06:05:43 -0700

    Update provider.py

--- ai/provider.py
+++ ai/provider.py
@@ -1,21 +1,28 @@
 """
+Project Nexus
+
 Provider Manager
 """
 
 from ai.providers.gemini import GeminiProvider
+from utils.logger import logger
 
 
 class ProviderManager:
 
     def __init__(self):
 
+        logger.info("Loading Gemini Provider...")
+
         self.provider = GeminiProvider()
+
+        logger.info("Gemini Provider Loaded.")
 
     async def ask(
         self,
         user_id: int,
         conversation: list,
-    ):
+    ) -> str:
 
         return await self.provider.ask(
             user_id=user_id,
