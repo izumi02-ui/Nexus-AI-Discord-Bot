2c4910131bb63c7d69575a7c0caa16ab8a1701bb
izumi02-ui <rohitchakraborty0555@gmail.com>
2026-07-08 03:13:51 -0700

    Update gemini.py

--- ai/providers/gemini.py
+++ ai/providers/gemini.py
@@ -4,10 +4,24 @@ Project Nexus
 Gemini Provider
 """
 
+from google import genai
+
+from utils.settings import settings
 from ai.providers.base import BaseProvider
+from utils.logger import logger
 
 
 class GeminiProvider(BaseProvider):
+
+    def __init__(self):
+
+        logger.info("Initializing Gemini...")
+
+        self.client = genai.Client(
+            api_key=settings.gemini_api_key
+        )
+
+        logger.info("Gemini Ready.")
 
     async def ask(
         self,
@@ -15,8 +29,27 @@ class GeminiProvider(BaseProvider):
         conversation: list,
     ) -> str:
 
-        return (
-            "Gemini Provider Ready\n"
-            f"User: {user_id}\n"
-            f"Messages: {len(conversation)}"
-        )
+        try:
+
+            prompt = ""
+
+            for message in conversation:
+
+                role = message["role"]
+
+                content = message["content"]
+
+                prompt += f"{role.upper()}:\n{content}\n\n"
+
+            response = self.client.models.generate_content(
+                model=settings.model,
+                contents=prompt
+            )
+
+            return response.text
+
+        except Exception as error:
+
+            logger.error(error)
+
+            raise
