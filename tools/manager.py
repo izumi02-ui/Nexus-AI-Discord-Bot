87b5635bb3e3eedf19e8a81e0836834bafe71334
izumi02-ui <rohitchakraborty0555@gmail.com>
2026-07-09 06:43:42 -0700

    Update manager.py

--- tools/manager.py
+++ tools/manager.py
@@ -5,6 +5,25 @@ Tool Manager
 """
 
 from tools.google_search import google_search
+from tools.duckduckgo import duckduckgo
+from tools.wikipedia import wikipedia
+from tools.reddit import reddit
+from tools.github import github
+from tools.youtube import youtube
+from tools.news import news
+from tools.stackoverflow import stackoverflow
+from tools.arxiv import arxiv
+from tools.weather import weather
+from tools.maps import maps
+from tools.currency import currency
+from tools.time import time_tool
+from tools.translator import translator
+from tools.spotify import spotify
+from tools.steam import steam
+from tools.ocr import ocr
+from tools.vision import vision
+from tools.file_reader import file_reader
+from tools.web_scraper import web_scraper
 
 from utils.logger import logger
 
@@ -13,14 +32,42 @@ class ToolManager:
 
     def __init__(self):
 
-        self.tools = {
+        self.tools = {}
 
-            google_search.name: google_search,
-
-        }
+        self._register(google_search)
+        self._register(duckduckgo)
+        self._register(wikipedia)
+        self._register(reddit)
+        self._register(github)
+        self._register(youtube)
+        self._register(news)
+        self._register(stackoverflow)
+        self._register(arxiv)
+        self._register(weather)
+        self._register(maps)
+        self._register(currency)
+        self._register(time_tool)
+        self._register(translator)
+        self._register(spotify)
+        self._register(steam)
+        self._register(ocr)
+        self._register(vision)
+        self._register(file_reader)
+        self._register(web_scraper)
 
         logger.info(
-            "Tool Manager Ready."
+            f"Loaded {len(self.tools)} tools."
+        )
+
+    def _register(
+        self,
+        tool,
+    ):
+
+        self.tools[tool.name] = tool
+
+        logger.info(
+            f"Loaded Tool: {tool.name}"
         )
 
     async def execute(
@@ -29,24 +76,31 @@ class ToolManager:
         query: str,
     ):
 
-        tool_instance = self.tools.get(
+        tool = self.tools.get(
             tool
         )
 
-        if tool_instance is None:
+        if tool is None:
 
             raise ValueError(
                 f"Unknown tool: {tool}"
             )
 
-        return await tool_instance.execute(
+        return await tool.execute(
             query
         )
+
+    def get(
+        self,
+        name: str,
+    ):
+
+        return self.tools.get(name)
 
     @property
     def available(self):
 
-        return list(
+        return sorted(
             self.tools.keys()
         )
 
