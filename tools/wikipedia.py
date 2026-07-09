a9996014fab1a405524eede50188702ff7ef101f
izumi02-ui <rohitchakraborty0555@gmail.com>
2026-07-09 06:09:46 -0700

    Update wikipedia.py

--- tools/wikipedia.py
+++ tools/wikipedia.py
@@ -1,1 +1,31 @@
+"""
+Project Nexus
 
+Wikipedia Search
+"""
+
+from search.search_result import SearchResult
+
+
+class WikipediaTool:
+
+    @property
+    def name(self):
+
+        return "wikipedia"
+
+    async def execute(
+        self,
+        query: str,
+    ):
+
+        # TODO
+        return SearchResult(
+            title="Wikipedia",
+            content="Coming soon.",
+            source="Wikipedia",
+            confidence=0.70,
+        )
+
+
+wikipedia = WikipediaTool()
