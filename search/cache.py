32b2b9476e85d66c8e7203768e83a8cfbeb3a9be
izumi02-ui <rohitchakraborty0555@gmail.com>
2026-07-09 06:08:15 -0700

    Update cache.py

--- search/cache.py
+++ search/cache.py
@@ -1,1 +1,50 @@
+"""
+Project Nexus
 
+Search Cache
+"""
+
+from time import time
+
+
+class SearchCache:
+
+    def __init__(self):
+
+        self.cache = {}
+
+    def get(
+        self,
+        key: str,
+    ):
+
+        item = self.cache.get(key)
+
+        if item is None:
+
+            return None
+
+        expires, value = item
+
+        if time() > expires:
+
+            del self.cache[key]
+
+            return None
+
+        return value
+
+    def set(
+        self,
+        key: str,
+        value,
+        ttl: int = 300,
+    ):
+
+        self.cache[key] = (
+            time() + ttl,
+            value
+        )
+
+
+cache = SearchCache()
