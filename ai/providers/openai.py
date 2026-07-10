cf1d0e7a22d9ae527d7dc32df57b0c8d9896a9e3
izumi02-ui <rohitchakraborty0555@gmail.com>
2026-07-10 00:13:30 -0700

    Update openai.py

--- ai/providers/openai.py
+++ ai/providers/openai.py
@@ -1,1 +1,75 @@
+"""
+Project Nexus
 
+OpenAI Provider
+"""
+
+from typing import List, Dict
+
+from openai import OpenAI
+
+from ai.provider_capabilities import ProviderCapabilities
+from ai.providers.base import BaseProvider
+
+from utils.logger import logger
+from utils.settings import settings
+
+
+class OpenAIProvider(BaseProvider):
+
+    @property
+    def name(self) -> str:
+
+        return "OpenAI"
+
+    @property
+    def capabilities(self):
+
+        return ProviderCapabilities(
+            web_search=False,
+            vision=True,
+            files=True,
+            function_calling=True,
+            image_generation=True,
+            code_execution=False,
+        )
+
+    def __init__(self):
+
+        logger.info(
+            "Initializing OpenAI..."
+        )
+
+        self.client = OpenAI(
+            api_key=settings.openai_api_key
+        )
+
+        logger.info(
+            "OpenAI Ready."
+        )
+
+    async def ask(
+        self,
+        user_id: int,
+        conversation: List[Dict],
+    ) -> str:
+
+        response = self.client.chat.completions.create(
+
+            model=settings.model,
+
+            messages=conversation,
+
+        )
+
+        logger.info(
+            f"{self.name} replied to {user_id}"
+        )
+
+        return (
+            response
+            .choices[0]
+            .message
+            .content
+            .strip()
+        )
